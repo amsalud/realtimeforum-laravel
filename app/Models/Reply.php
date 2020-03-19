89758783515e3c33857b6c4c101750aac51848b9
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-18 21:35:56 -0400

    Define reply model relationships

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function question(){
+        return $this->belongsTo('App\Models\Question');
+    }
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function like(){
+        return $this->hasMany('App\Models\Like');
+    }
 }

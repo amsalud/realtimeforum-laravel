ed5bdeb69daa2e274db329e1524b3351171fd54f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-19 15:36:03 -0400

    Add functionality to delete a category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -76,6 +76,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }

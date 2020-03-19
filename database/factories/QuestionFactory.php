bb9f13f38d3be0af67b5bb90e9f7f145781cbfa9
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-19 11:49:25 -0400

    Update QuestionFactory.php

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -3,10 +3,20 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Question;
+use App\Models\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title' => $title,
+        'slug' => str_slug($title),
+        'body' => $faker->text,
+        'category_id' => function(){
+            return Category::all()->random();
+        },
+        'user_id' => function(){
+            return User::all()->random();
+        }
     ];
 });

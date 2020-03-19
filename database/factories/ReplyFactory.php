cfafffd40a3b2d1e3b58d652b3cc6f730f9a600b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-19 11:49:21 -0400

    Update ReplyFactory.php

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -3,10 +3,17 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Reply;
+use App\Models\Question;
 use Faker\Generator as Faker;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function(){
+            return Question::all()->random();
+        },
+        'user_id' => function(){
+            return User::all()->random();
+        }
     ];
 });

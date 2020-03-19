b2e0a71a45f8a47c015a0285cd09089fffb26063
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-18 21:36:04 -0400

    Define question model relationships

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user(){
+        $this->belongsTo('App\User');
+    }
+
+    public function replies(){
+        return $this->hasMany('App\Models\Reply');
+    }
+
+    public function category(){
+        return $this->belongsTo('App\Models\Category');
+    }
 }

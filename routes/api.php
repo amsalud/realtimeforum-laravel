eb5fbadace1bdf3a147d066e9e9999052f287bfd
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-20 21:52:25 -0400

    Add route for signing up

--- routes/api.php
+++ routes/api.php
@@ -35,5 +35,7 @@ Route::group([
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
 
 });

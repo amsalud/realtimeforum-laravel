db6a6a9fa5f4e8146579ca16fa96df7d914e3b94
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-20 21:15:05 -0400

    Add auth routes

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,17 @@ Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
 Route::post('/like/{reply}', 'LikeController@likeReply');
 Route::delete('/like/{reply}', 'LikeController@unlikeReply');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});

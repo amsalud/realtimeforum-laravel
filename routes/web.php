daeb7f48eb602f13df2973cbfac56d05763f1b59
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-21 22:59:29 -0400

    Fix Not Found error navigating through components and refreshing page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{vue_capture?}', function () {
     return view('app');
-});
+})->where('vue_capture', '[\/\w\.-]*');

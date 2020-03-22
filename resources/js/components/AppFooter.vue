a19669ccd5b7efeea361f26e4018507ff0791541
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-21 22:09:09 -0400

    Create AppFooter.vue

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-footer absolute class="font-weight-medium">
+      <v-col class="text-center" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Laravel Forum</strong>
+      </v-col>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name: 'AppFooter'
+}
+</script>

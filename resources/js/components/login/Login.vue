84a25c17105a6a410882d78372379d00267f29a0
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-21 22:40:49 -0400

    Create Login.vue

---
+++ resources/js/components/login/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login-container">
+        <h1>Login Component</h1> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+
+<style scoped>
+    .login-container{
+        margin-top:45px;
+    }
+</style>

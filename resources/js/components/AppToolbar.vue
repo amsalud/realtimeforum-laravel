1aee6c6d3b3d626a6f7f2020b9b97403779aa519
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-21 22:04:01 -0400

    Create AppToolbar.vue

---
+++ resources/js/components/AppToolbar.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <v-app-bar
+      absolute
+      color="white"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+    >
+      <v-toolbar-title>Laravel Forum</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "AppToolbar"
+}
+</script>

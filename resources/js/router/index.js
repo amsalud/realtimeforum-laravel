c33bee3aa1ce7e5a875c214d50dda4e9449f1c5f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-21 22:41:13 -0400

    Add login route

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Login from '../components/login/Login'
+
 Vue.use(VueRouter)
 
 const routes = [
-    // { path: '/login', component: Login },
+    { path: '/login', component: Login },
   ]
   
 export default new VueRouter({routes})

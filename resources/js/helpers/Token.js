1ff984191d3786fc93c6ef2baa2fc7a60765b844
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-22 22:22:24 -0400

    Create Token.js

---
+++ resources/js/helpers/Token.js
@@ -0,0 +1,19 @@
+class Token {
+    isValid(token){
+        const payload = this.payload(token);
+        if(payload){
+            return payload.iss == "http://localhost:8000/api/auth/login" ? true : false;
+        }
+        return false;
+    }
+    payload(token){
+      const tokenPayload = token.split('.')[1]; 
+      return this.decode(tokenPayload);
+    }
+    decode(tokenPayload){
+        return JSON.parse(atob(tokenPayload));
+    }
+
+}
+
+export default new Token();

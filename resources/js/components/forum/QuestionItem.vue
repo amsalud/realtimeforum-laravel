3278d266ab2a95ab10a96721422b2abe646d9b68
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-23 15:55:27 -0400

    Create QuestionItem.vue

---
+++ resources/js/components/forum/QuestionItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-card class="mx-auto mt-3 mb-3">
+        <v-card-title>{{question.title}}</v-card-title>
+        <v-card-subtitle class="pb-0 grey--text">{{question.user}} said {{question.created_at}}</v-card-subtitle>
+        <v-card-text class="text--primary">
+            <div>{{question.body}}</div>
+        </v-card-text>
+    </v-card>
+</template>
+<script>
+export default {
+    name: "QuestionItem",
+    props: ['question']
+}
+</script>

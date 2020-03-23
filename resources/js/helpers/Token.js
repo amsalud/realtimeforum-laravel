49d20bccb9006eeb1c172322fe46ca0623cdd5a0
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-23 13:06:17 -0400

    Fix issue with signing in newly created user

--- resources/js/helpers/Token.js
+++ resources/js/helpers/Token.js
@@ -2,11 +2,12 @@ class Token {
     isValid(token){
         const payload = this.payload(token);
         if(payload){
-            return payload.iss == "http://localhost:8000/api/auth/login" ? true : false;
+            console.log(payload.iss);
+            return payload.iss == "http://localhost:8000/api/auth/login" || "http://localhost:8000/api/auth/signup" ? true : false;
         }
         return false;
     }
-    
+
     payload(token){
       const tokenPayload = token.split('.')[1]; 
       return this.decode(tokenPayload);

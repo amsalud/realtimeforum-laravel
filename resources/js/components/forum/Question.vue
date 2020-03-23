825b61d4d29d8a3d1f31df46af5ff8354d83b2d1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-23 15:46:57 -0400

    Define question prop

--- resources/js/components/forum/Question.vue
+++ resources/js/components/forum/Question.vue
@@ -1,14 +1,15 @@
 <template>
     <v-card class="mx-auto mt-3 mb-3">
-        <v-card-title>Question Title</v-card-title>
-        <v-card-subtitle class="pb-0 grey--text">Said x hour ago</v-card-subtitle>
+        <v-card-title>{{question.title}}</v-card-title>
+        <v-card-subtitle class="pb-0 grey--text">{{question.user}} said {{question.created_at}}</v-card-subtitle>
         <v-card-text class="text--primary">
-            <div>Question Content</div>
+            <div>{{question.body}}</div>
         </v-card-text>
     </v-card>
 </template>
 <script>
 export default {
-    name: "Question"
+    name: "Question",
+    props: ['question']
 }
 </script>

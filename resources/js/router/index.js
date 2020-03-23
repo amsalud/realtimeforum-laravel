40a5deb3b1715c334b10fff2d1ce78ab4158a4e7
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-22 22:51:12 -0400

    Add new route for sign up page

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Login from '../components/login/Login'
+import SignUp from '../components/signup/SignUp'
 
 Vue.use(VueRouter)
 
 const routes = [
     { path: '/login', component: Login },
+    { path: '/register', component: SignUp },
   ]
   
 export default new VueRouter({ routes, mode: 'history'})

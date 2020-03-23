486045c219d249c78c68f7a78555801499c5bf15
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-23 13:48:03 -0400

    Add configured menu buttons for login and logout

--- resources/js/components/AppToolbar.vue
+++ resources/js/components/AppToolbar.vue
@@ -9,16 +9,26 @@
       <v-toolbar-title>Laravel Forum</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-          <v-btn text medium to="/forum">Forum</v-btn>
-          <v-btn text medium to="/question">Ask Question</v-btn>
-          <v-btn text medium to="/category">Category</v-btn>
-          <v-btn text medium to="/login">Sign In</v-btn>
+          <v-btn  v-for="(item, index) in items" :key="index" :to="item.to" text medium v-show="item.show">{{item.title}}</v-btn>
       </v-toolbar-items>
     </v-app-bar>
 </template>
 
 <script>
+
+import User from '../helpers/User'
 export default {
-    name: "AppToolbar"
+    name: "AppToolbar",
+    data(){
+      return {
+        items: [
+          {title: 'Forum', to: '/forum', show: true},
+          {title: 'Ask Question', to: '/question', show: User.loggedIn()},
+          {title: 'Category', to: '/category', show: User.loggedIn()},
+          {title: 'Sign in', to: '/login', show: !User.loggedIn()},
+          {title: 'Sign Out', to: '/logout', show: User.loggedIn()}
+        ]
+      }
+    }
 }
 </script>

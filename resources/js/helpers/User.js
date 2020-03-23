a70a2d14bd44d1e43044e25f2793001c772976aa
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-22 22:29:28 -0400

    Add method to handle auth token after logging in

--- resources/js/helpers/User.js
+++ resources/js/helpers/User.js
@@ -1,12 +1,19 @@
 import Token from './Token'
+import AppStorage from './AppStorage'
 
 class User {
     login(formData) {
         axios.post('/api/auth/login', formData)
         .then(res=> {
-            Token.payload(res.data.access_token);
+            this.responseAfterLogin(res.data);
         })
         .catch(error=> console.log(error.response.data));
+    }
+    responseAfterLogin(data){
+        const {user, access_token } = data;
+        if(Token.isValid(access_token)){
+            AppStorage.store(user, access_token)
+        }
     }
 }
 

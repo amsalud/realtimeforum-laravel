220802ce8dc2cdfd5f6bd8871fb682bb96199c70
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-22 23:09:58 -0400

    Add input validation to Signup form

--- resources/js/components/signup/SignUp.vue
+++ resources/js/components/signup/SignUp.vue
@@ -6,12 +6,14 @@
         v-model="form.name"
         label="Name"
         type="text"
+        :rules="inputRules.name"
         required
       ></v-text-field>
       <v-text-field
         v-model="form.email"
         label="Email"
         type="email"
+        :rules="inputRules.email"
         required
       ></v-text-field>
 
@@ -19,12 +21,14 @@
         v-model="form.password"
         label="Password"
         type="password"
+        :rules="inputRules.password"
         required
       ></v-text-field>
         <v-text-field
         v-model="form.password_confirmation"
         label="Confirm Password"
         type="password"
+        :rules="inputRules.password_confirmation"
         required
       ></v-text-field>
       <v-btn
@@ -52,6 +56,23 @@ export default {
                 email: null,
                 password: null,
                 password_confirmation: null
+            },
+            inputRules:{
+                name: [
+                    v => !!v || 'Name is required',
+                    v => v.length > 5 || 'Name must be greater than 5 characters'
+                ],
+                email: [
+                    v => !!v || 'E-mail is required',
+                    v => /.+@.+/.test(v) || 'E-mail must be valid',
+                ],
+                password: [
+                    v => !!v || 'Password is required'
+                ],
+                password_confirmation: [
+                    v => !!v || 'Password Confirmation is required',
+                    v => v != this.form.password
+                ]
             }
         }
     },

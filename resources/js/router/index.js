bd6e9e51f5f3468a98569291129e41b6e307f880
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-23 15:00:40 -0400

    Add logout route

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 import Login from '../components/login/Login'
 import SignUp from '../components/signup/SignUp'
 import Forum from '../components/forum/Forum'
+import Logout from '../components/logout/Logout'
 
 Vue.use(VueRouter)
 
@@ -11,6 +12,7 @@ const routes = [
     { path: '/login', component: Login },
     { path: '/register', component: SignUp },
     { path: '/forum', component: Forum, name: 'forum' },
+    { path: '/logout', component: Logout, name: 'logout' },
   ]
   
 export default new VueRouter({ routes, mode: 'history'})

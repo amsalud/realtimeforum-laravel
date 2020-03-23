efd1c63a7d850a5e7ff167788d14434c116178de
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-23 18:49:31 -0400

    Add question view ui layout

--- resources/js/components/forum/Question.vue
+++ resources/js/components/forum/Question.vue
@@ -1,9 +1,33 @@
 <template>
-    
+    <v-card>
+        <v-container fluid>
+            <v-card-title>
+                <div>
+                    <div class="headline">
+                        {{question.title}}
+                    </div>
+                    <span class="grey--text">{{question.user}} said {{question.created_at}}</span>
+                </div>
+                <v-spacer></v-spacer>
+                <v-btn outlined>5 Replies</v-btn>
+            </v-card-title>
+            <v-card-text v-html="question.body"></v-card-text>
+        </v-container>
+    </v-card>
 </template>
 
 <script>
 export default {
-    name: "Question"
+    name: "Question",
+    data(){
+        return {
+            question: {}
+        }
+    },
+    created(){
+        axios.get(`/api/question/${this.$route.params.slug}`)
+        .then(res=>this.question = res.data.data)
+        .catch(err=> console.log(err));
+    }
 }
 </script>

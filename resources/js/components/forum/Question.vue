a3dc9274e8e9712f5e0c420d63fa89123c9554d0
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-23 15:31:17 -0400

    Create Question.vue

---
+++ resources/js/components/forum/Question.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-card class="mx-auto mt-3 mb-3">
+        <v-card-title>Question Title</v-card-title>
+        <v-card-subtitle class="pb-0 grey--text">Said x hour ago</v-card-subtitle>
+        <v-card-text class="text--primary">
+            <div>Question Content</div>
+        </v-card-text>
+    </v-card>
+</template>
+<script>
+export default {
+    name: "Question"
+}
+</script>

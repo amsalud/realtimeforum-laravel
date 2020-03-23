b21948d22136f1923559367390cd0485541c75b3
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-23 15:31:28 -0400

    Render Question component

--- resources/js/components/forum/Forum.vue
+++ resources/js/components/forum/Forum.vue
@@ -1,9 +1,22 @@
 <template>
-    <h1>Forum</h1>
+    <v-container fluid grid-list-md>
+        <v-layout>
+            <v-flex xs8>
+                <question></question>
+            </v-flex>
+            <v-flex>
+                Sidebar
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
+import Question from './Question'
 export default {
-    name: "Forum"
+    name: "Forum",
+    components:{
+        Question
+    }
 }
 </script>

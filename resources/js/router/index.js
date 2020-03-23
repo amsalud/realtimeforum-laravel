2cd5c1020703d2127438cbeae4ba18f27429623b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-23 13:11:58 -0400

    Add route for forum page

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,12 +3,14 @@ import VueRouter from 'vue-router'
 
 import Login from '../components/login/Login'
 import SignUp from '../components/signup/SignUp'
+import Forum from '../components/forum/Forum'
 
 Vue.use(VueRouter)
 
 const routes = [
     { path: '/login', component: Login },
     { path: '/register', component: SignUp },
+    { path: '/forum', component: Forum },
   ]
   
 export default new VueRouter({ routes, mode: 'history'})

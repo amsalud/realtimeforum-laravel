9ed064775b2405c0ddb64d4f61e74842f0426db4
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-23 15:56:22 -0400

    Update Question.vue

--- resources/js/components/forum/Question.vue
+++ resources/js/components/forum/Question.vue
@@ -1,15 +1,9 @@
 <template>
-    <v-card class="mx-auto mt-3 mb-3">
-        <v-card-title>{{question.title}}</v-card-title>
-        <v-card-subtitle class="pb-0 grey--text">{{question.user}} said {{question.created_at}}</v-card-subtitle>
-        <v-card-text class="text--primary">
-            <div>{{question.body}}</div>
-        </v-card-text>
-    </v-card>
+    
 </template>
+
 <script>
 export default {
-    name: "Question",
-    props: ['question']
+    name: "Question"
 }
 </script>

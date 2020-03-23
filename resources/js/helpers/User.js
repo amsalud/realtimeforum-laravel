5989636224607bf7bf89ccd551eb369ac6b9eba5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-22 21:56:22 -0400

    Add user helper methods

---
+++ resources/js/helpers/User.js
@@ -0,0 +1,9 @@
+const User = {
+    login(formData) {
+        axios.post('/api/auth/login', formData)
+        .then(res=> console.log(res.data))
+        .catch(error=> console.log(error.response.data));
+    }
+}
+
+export default User;

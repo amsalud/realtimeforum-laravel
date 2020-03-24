b74f0a18d7647504ea702684fcde1ea8732c84ce
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-23 20:01:51 -0400

    Create CreateQuestion.vue

---
+++ resources/js/components/forum/CreateQuestion.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-container>
+        <v-form @submit.prevent="true">
+      <v-text-field
+        v-model="form.email"
+        label="Title"
+        type="text"
+      ></v-text-field>
+
+    <v-autocomplete
+        v-model="form.category_id"
+        :items="categories"
+        item-text="name"
+        item-value="id"
+        label="Category"
+        placeholder="Select a Category"
+      ></v-autocomplete>
+
+      <v-btn
+        color="success"
+        class="mr-4"
+        type="submit"     
+      >Ask</v-btn>
+    </v-form>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "CreateQuestion",
+    data(){
+        return {
+            form: {
+                title: null,
+                category: null
+            },
+            categories: null
+        }
+    },
+    created(){
+        axios.get('/api/category')
+        .then(res=> this.categories = res.data.data)
+        .catch(err=>console.log(err));
+    }
+}
+</script>

7e5a87f0436a43723e8bd5453715c656b5f731f2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-24 21:16:35 -0400

    Create Category.vue

---
+++ resources/js/components/category/Category.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container>
+        <h1>Create a Category</h1>
+        <v-form @submit.prevent="handleSubmit">
+            <v-text-field
+            label="Name"
+            v-model="form.name"
+            >
+            </v-text-field>
+            <v-btn type="submit" color="success">Submit</v-btn>
+        </v-form>
+        <category-list :allowEditing="true"></category-list>
+    </v-container>
+    
+</template>
+
+<script>
+import CategoryList from './CategoryList'
+export default {
+    name: "Category",
+    components:{
+        CategoryList
+    },
+    data(){
+        return {
+            form: {}
+        }
+    },
+    methods: {
+        handleSubmit(){
+            this.createCategory();
+        },
+        createCategory(){
+            axios.post('/api/category', this.form)
+            .then(res=> this.$router.push('/forum'))
+            .catch(err=> console.log(err));
+        }
+    }
+}
+</script>

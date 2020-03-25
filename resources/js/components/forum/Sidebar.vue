ec0a9a170b179313bf8613e95f0a98bc109437f1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-24 20:31:56 -0400

    Create Sidebar.vue

---
+++ resources/js/components/forum/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-card class="mt-3">
+        <v-toolbar>
+            <v-toolbar-title>Categories</v-toolbar-title>
+        </v-toolbar>
+        <v-list>
+            <v-list-item v-for="(item, index) in categories" :key="index">
+                <v-list-item-content>
+                    <v-list-item-title>{{item.name}}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "Sidebar",
+    data(){
+        return {
+            categories:null
+        }
+    },
+    created(){
+        axios.get('/api/category')
+        .then(res=>this.categories = res.data.data)
+        .catch(err=>console.log(err));
+    }
+}
+</script>

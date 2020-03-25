f593a817224ae49418d0ce07c948eef7ced35d8b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-24 20:43:40 -0400

    Create CategoryForm.vue

---
+++ resources/js/components/category/CategoryForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <h1>Create a Category</h1>
+        <v-form>
+            <v-text-field
+            label="Name"
+            v-model="form.category"
+            >
+        </v-text-field>
+        <v-btn type="submit" color="success">Submit</v-btn>
+    </v-form>
+    </v-container>
+    
+</template>
+
+<script>
+export default {
+    name: "CategoryForm",
+    data(){
+        return {
+            form: {}
+        }
+    }
+}
+</script>

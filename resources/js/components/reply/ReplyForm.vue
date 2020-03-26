8e87ab636f949c5261c91317e9e76c35d0b3a292
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-25 22:52:36 -0400

    Add functionality to edit a reply from reply edit page

--- resources/js/components/reply/ReplyForm.vue
+++ resources/js/components/reply/ReplyForm.vue
@@ -1,6 +1,7 @@
 <template>
     <v-container>
-        <h1 class="mb-3">Post a reply</h1>
+        <h1 v-if="mode == 'create'" class="mb-3">Post a reply</h1>
+        <h1 v-if="mode == 'edit'" class="mb-3">Edit a reply</h1>
         <v-form @submit.prevent="handleSubmit">
             <vue-simplemde v-model="form.body" ref="markdownEditor" />
             <v-btn class="success" type="submit">Submit</v-btn>
@@ -20,12 +21,29 @@ export default {
         return{
             form: {
                 body: ''
-            }
+            },
+            mode: 'create'
+        }
+    },
+    created(){
+        if(this.$route.params && this.$route.params.slug && this.$route.params.reply_id){
+            this.mode = 'edit';
+            const {slug, reply_id} = this.$route.params;
+            axios.get(`/api/question/${slug}/reply/${reply_id}`)
+            .then((res)=> this.form.body = res.data.data.reply)
+            .catch(err=>console.log(err));
         }
     },
     methods: {
         handleSubmit(){
-            this.postReply();
+            switch(this.mode){
+                case 'create':
+                    this.postReply();
+                    break;
+                case 'edit':
+                    this.editReply();
+                    break;
+            }
         },
         postReply(){
             if(this.form.body && this.form.body.length){
@@ -35,6 +53,15 @@ export default {
                     EventBus.$emit('reply-created', res.data.reply);
                 })
                 .catch(err=> console.log(err));
+            }
+        },
+        editReply(){
+            const {slug, reply_id} = this.$route.params;
+
+            if(this.form.body && this.form.body.length){
+                axios.put(`/api/question/${slug}/reply/${reply_id}`, this.form)
+                .then((res)=>this.$router.push(`/question/${slug}`))
+                .catch(err=>console.log(err));
             }
         }
     }

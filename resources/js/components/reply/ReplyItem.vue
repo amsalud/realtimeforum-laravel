a094b35dd8ebbabeca6dde3f57d9e47d98ffa405
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-25 20:31:37 -0400

    Add reply content to each reply item

--- resources/js/components/reply/ReplyItem.vue
+++ resources/js/components/reply/ReplyItem.vue
@@ -2,8 +2,9 @@
     <v-card class="ma-3">
         <v-card-title>
             <div class="headline">{{reply.user}}</div>
-             <div class="ml-2">{{reply.created_at}}</div>
+             <div class="ml-2">said {{reply.created_at}}</div>
         </v-card-title>
+        <v-card-text v-html="reply.reply"></v-card-text>
     </v-card>
 </template>
 

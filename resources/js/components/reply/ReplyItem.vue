1e119fd8917f744402b21a3593a5868b9d3bc9c8
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-25 20:39:30 -0400

    Add edit and delete button for reply items

--- resources/js/components/reply/ReplyItem.vue
+++ resources/js/components/reply/ReplyItem.vue
@@ -2,8 +2,17 @@
     <v-card class="ma-3">
         <v-card-title>
             <div class="headline">{{reply.user}}</div>
-             <div class="ml-2">said {{reply.created_at}}</div>
+            <div class="ml-2">said {{reply.created_at}}</div>
+            <v-card-actions class="align-end">
+                <v-btn icon small >
+                    <v-icon>edit</v-icon>
+                </v-btn>
+                <v-btn icon small>
+                    <v-icon>delete</v-icon>
+                </v-btn>
+            </v-card-actions>
         </v-card-title>
+         
         <v-card-text v-html="reply.reply"></v-card-text>
     </v-card>
 </template>

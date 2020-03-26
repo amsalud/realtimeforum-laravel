e2edb5441e78e86f761bee5fef5cd640bf10349e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-26 19:49:01 -0400

    Create ReplyLike.vue

---
+++ resources/js/components/reply/ReplyLike.vue
@@ -0,0 +1,11 @@
+<template>
+        <v-btn icon small>
+            <v-icon color="red">favorite</v-icon>
+        </v-btn>
+</template>
+
+<script>
+export default {
+    name: "ReplyLike"
+}
+</script>

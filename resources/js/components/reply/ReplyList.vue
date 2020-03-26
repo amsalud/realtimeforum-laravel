fc8af4d5358dd0fd7c5ec236602732511421cf52
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-25 20:23:10 -0400

    Create ReplyList.vue

---
+++ resources/js/components/reply/ReplyList.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container>
+        <reply-item v-for="(item, index) in replies" :key="index" :reply="item"></reply-item>
+    </v-container>
+</template>
+
+<script>
+import ReplyItem from './ReplyItem'
+export default {
+    name: "ReplyList",
+    components: {
+        ReplyItem
+    }, 
+    props: ['replies']
+}
+</script>

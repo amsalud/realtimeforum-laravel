e56cf8705b655ec95782e5018d4a35cd5a6ed07d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-26 19:49:29 -0400

    Add Likes to Reply Resource payload

--- app/Http/Resources/ReplyResource.php
+++ app/Http/Resources/ReplyResource.php
@@ -19,6 +19,7 @@ class ReplyResource extends JsonResource
             'reply' => $this->body,
             'user' => $this->user->name,
             'user_id' => $this->user->id,
+            'likes' =>$this->like,
             'created_at' => $this->created_at->diffForHumans()
         ];
     }

4c78eb7f8ddf569909192749a28e8c73626e60d0
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-25 20:23:15 -0400

    Create ReplyItem.vue

---
+++ resources/js/components/reply/ReplyItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-card class="ma-3">
+        <v-card-title>
+            <div class="headline">{{reply.user}}</div>
+             <div class="ml-2">{{reply.created_at}}</div>
+        </v-card-title>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "ReplyItem",
+    props: ['reply']
+}
+</script>

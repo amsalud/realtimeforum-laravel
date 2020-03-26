09710cb55dd1aeb024fbcc920f1f7adfe12e3078
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-25 22:52:15 -0400

    Add link to edit a reply

--- resources/js/components/reply/ReplyItem.vue
+++ resources/js/components/reply/ReplyItem.vue
@@ -4,7 +4,7 @@
             <div class="headline">{{reply.user}}</div>
             <div class="grey--text ml-2"> said {{reply.created_at}}</div>
             <v-card-actions v-if="own" class="align-end ml-auto">
-                <v-btn icon small >
+                <v-btn icon small :to="`/question/${question_slug}/reply/${reply.id}/edit`">
                     <v-icon>edit</v-icon>
                 </v-btn>
                 <v-btn icon small @click="deleteReply">

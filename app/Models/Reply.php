84bb7ee7b1c943fd3638242b1db004f2f8f3ea50
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-25 21:19:39 -0400

    Prefill currently logged in user_id upon reply creation

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     protected $guarded = [];
+
+    protected static function boot(){
+        parent::boot();
+
+        static::creating(function($reply){
+            $reply->user_id = auth()->id();
+        });
+    }
     
     public function question(){
         return $this->belongsTo('App\Models\Question');

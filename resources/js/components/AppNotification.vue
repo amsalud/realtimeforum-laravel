d6a8953a789e7e9d8ff45262c050d9866dc968a2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-26 22:16:39 -0400

    Display unread notifications

--- resources/js/components/AppNotification.vue
+++ resources/js/components/AppNotification.vue
@@ -4,18 +4,49 @@
       <template v-slot:activator="{ on }">
          <v-btn icon>
             <v-icon v-on="on">notifications</v-icon>
+            {{unreadCount ? unreadCount : ''}}
         </v-btn>
       </template>
       <v-list>
-        <v-list-item>
-          <v-list-item-title>Notification</v-list-item-title>
+        <v-list-item v-for="item in unread" :key="item.id">
+        <router-link text :to="`/question/${item.data.slug}`" tag="v-list-item-title">
+           {{item.data.question}}
+        </router-link>
         </v-list-item>
       </v-list>
     </v-menu>
   </div>
 </template>
 <script>
+import User from '../helpers/User';
 export default {
-    name: "AppNotification"
+    name: "AppNotification",
+    data(){
+        return{
+            read: [],
+            unread: [],
+            unreadCount: 0
+        }
+    },
+    created(){
+        if(User.loggedIn()){
+            this.getNotifications();
+        }
+    },
+    methods: {
+        getNotifications(){
+            axios.post('/api/notifications')
+            .then(res=>{
+                this.read = res.data.read;
+                this.unread = res.data.unread;
+                this.unreadCount = res.data.unread.length;
+            })
+        }
+    }
 }
 </script>
+<style  scoped>
+    .v-list-item{
+        cursor:pointer;
+    }
+</style>

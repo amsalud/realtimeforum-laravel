6edb6c3e0a23ce7e3e7003cf19c0a45b43167d31
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-26 20:26:57 -0400

    Prefill user_id when creating a like entry

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     protected $guarded = [];
+
+    protected static function boot(){
+        parent::boot();
+
+        static::creating(function($like){
+            $like->user_id = auth()->id();
+        });
+    }
 }

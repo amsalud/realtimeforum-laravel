2cf1aee3764477d0bc1731800b589d9673634b23
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-26 20:27:33 -0400

    Add functionality to Like and Unlike a reply

--- resources/js/components/reply/ReplyLike.vue
+++ resources/js/components/reply/ReplyLike.vue
@@ -1,11 +1,12 @@
 <template>
     <fragment>
-        <v-btn v-if="liked" icon small>
+        <v-btn v-if="liked" icon small @click="unlike">
             <v-icon color="red">favorite</v-icon>
         </v-btn>
-        <v-btn v-if="!liked" icon small>
+        <v-btn v-if="!liked" icon small @click="like">
             <v-icon>favorite</v-icon>
         </v-btn>    
+        {{this.reply.likes.length ? this.reply.likes.length : ''}}
     </fragment>
 </template>
 
@@ -24,17 +25,30 @@ export default {
         }
     },
     created(){
-        const userId = User.getId();
         if(this.reply.likes && this.reply.likes.length > 0){
-
+           if(this.reply.likes.find(like=> like.user_id == User.getId())){
+               this.liked = !this.liked;
+           }
         }
     },
     methods: {
         like(){
-
+            axios.post(`/api/like/${this.reply.id}`)
+            .then(res=> {
+                this.liked = !this.liked
+                this.reply.likes.push(res.data);
+            })
+            .catch(err=> console.log(err));
         },
         unlike(){
+             axios.delete(`/api/like/${this.reply.id}`)
+            .then(res=>{ 
+                this.liked = !this.liked
+                const index = this.reply.likes.findIndex(like=> like.user_id == User.getId());
+                this.reply.likes.splice(index, 1);
 
+            })
+            .catch(err=> console.log(err));
         }
     }
 }

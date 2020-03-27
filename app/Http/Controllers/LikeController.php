1d932cfdf2497b2fb848ba1a4384d63d1d0c708e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-26 20:27:20 -0400

    Return like data after like entry creation

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Like;
 use App\Models\Reply;
 use Illuminate\Http\Request;
-
+use Illuminate\Http\Response;
 
 class LikeController extends Controller
 {
@@ -15,14 +15,11 @@ class LikeController extends Controller
     }
 
     public function likeReply(Reply $reply){
-        $reply->like()->create([
-            // 'user_id' => auth()->id()
-            'user_id' => '1'
-        ]);
+        $like = $reply->like()->create();
+        return response($like, Response::HTTP_CREATED);
     }   
     
     public function unlikeReply(Reply $reply){
-        // $reply->like()->where('user_id',auth()->id())->first()->delete();
-        $reply->like()->where('user_id', '1')->first()->delete();
+        $reply->like()->where('user_id',auth()->id())->first()->delete();
     }
 }

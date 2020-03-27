7aa70e007c9c547eccd5a639598221e56685c204
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-26 20:02:43 -0400

    Add Like and Unlike button

--- resources/js/components/reply/ReplyLike.vue
+++ resources/js/components/reply/ReplyLike.vue
@@ -1,11 +1,41 @@
 <template>
-        <v-btn icon small>
+    <fragment>
+        <v-btn v-if="liked" icon small>
             <v-icon color="red">favorite</v-icon>
         </v-btn>
+        <v-btn v-if="!liked" icon small>
+            <v-icon>favorite</v-icon>
+        </v-btn>    
+    </fragment>
 </template>
 
 <script>
+import { Fragment } from 'vue-fragment'
+import User from '../../helpers/User'
 export default {
-    name: "ReplyLike"
+    name: "ReplyLike",
+    props: ['reply'],
+    components : {
+        Fragment
+    },
+    data(){
+        return {
+            liked: false
+        }
+    },
+    created(){
+        const userId = User.getId();
+        if(this.reply.likes && this.reply.likes.length > 0){
+
+        }
+    },
+    methods: {
+        like(){
+
+        },
+        unlike(){
+
+        }
+    }
 }
 </script>

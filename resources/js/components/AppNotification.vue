6a726609424139c0d486b73493a8935d1e60e422
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-26 21:28:07 -0400

    Add notification menu view

--- resources/js/components/AppNotification.vue
+++ resources/js/components/AppNotification.vue
@@ -1,7 +1,18 @@
 <template>
-    <v-btn icon>
-        <v-icon>notifications</v-icon>
-    </v-btn>
+    <div class="text-center">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on }">
+         <v-btn icon>
+            <v-icon v-on="on">notifications</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title>Notification</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
 </template>
 <script>
 export default {

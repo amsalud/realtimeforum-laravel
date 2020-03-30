cccea4bc3830185aa34d79f012363e97b8df608b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-30 10:52:12 -0400

    Fix footer style

--- resources/js/components/AppFooter.vue
+++ resources/js/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-footer absolute class="font-weight-medium">
+    <v-footer padless class="font-weight-medium">
       <v-col class="text-center" cols="12">
         {{ new Date().getFullYear() }} — <strong>Laravel Forum</strong>
       </v-col>

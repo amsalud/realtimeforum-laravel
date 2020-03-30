49e8a676daacb213779605af9ac58da91523d351
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-29 23:36:34 -0400

    Create HomePage.vue

---
+++ resources/js/components/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" height="1000">
+    
+        <v-layout column align-center justify-center>
+            <h1 class="white--text display-4">Laravel Forum</h1>
+            <h4>Try it now!</h4>
+        </v-layout>
+    </v-parallax>
+</template>
+
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>

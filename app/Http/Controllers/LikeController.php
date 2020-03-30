31bec0afde0ed712a48a6a5b4ce43e1e10124079
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-29 21:05:30 -0400

    Broadcast LikeEvents when liking and unliking a post

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\LikeEvent;
 use App\Models\Like;
 use App\Models\Reply;
 use Illuminate\Http\Request;
@@ -16,10 +17,13 @@ class LikeController extends Controller
 
     public function likeReply(Reply $reply){
         $like = $reply->like()->create();
+
+        broadcast(new LikeEvent($reply->id, 1))->toOthers();
         return response($like, Response::HTTP_CREATED);
     }   
     
     public function unlikeReply(Reply $reply){
         $reply->like()->where('user_id',auth()->id())->first()->delete();
+        broadcast(new LikeEvent($reply->id, 0))->toOthers();
     }
 }

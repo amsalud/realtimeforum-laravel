c36b2b0057cd268178a0e3bd1b6d0232a11688cf
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-29 21:04:55 -0400

    Define LikeEvent and LikeEventListener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,9 +15,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
+        'App\Events\LikeEvent' => [
+            'App\Listeners\LikeEventListener'
+        ]
     ];
 
     /**

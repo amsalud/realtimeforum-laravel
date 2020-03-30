d81306175020a9ae58f364ba82940c99f87e519c
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-30 19:27:45 -0400

    Disable like button if not logged in

--- resources/js/components/reply/ReplyLike.vue
+++ resources/js/components/reply/ReplyLike.vue
@@ -3,7 +3,7 @@
         <v-btn v-if="liked" icon small @click="unlike">
             <v-icon color="red">favorite</v-icon>
         </v-btn>
-        <v-btn v-if="!liked" icon small @click="like">
+        <v-btn v-if="!liked" icon small @click="like" :disabled="!userLoggedIn">
             <v-icon>favorite</v-icon>
         </v-btn>    
         {{this.reply.likes.length ? this.reply.likes.length : ''}}
@@ -21,7 +21,8 @@ export default {
     },
     data(){
         return {
-            liked: false
+            liked: false,
+            userLoggedIn: User.loggedIn()
         }
     },
     created(){
@@ -31,17 +32,19 @@ export default {
            }
         }
 
-        Echo.private('likeChannel')
-        .listen('.LikeEvent', (e) => {
-            if(this.reply.id == e.data.reply_id && e.type == 1){
-                this.reply.likes.push(e.data);
-            }else if(this.reply.id == e.data.reply_id && e.type == 0){
-                const index = this.reply.likes.findIndex(like=> like.user_id == e.data.user_id);
-                this.reply.likes.splice(index, 1);
-            }
-        });
+        this.setupEchoListeners();
     },
     methods: {
+        setupListeners(){
+            EventBus.$on('logout', ()=> { 
+                this.userLoggedIn = User.loggedIn();
+            });
+
+            EventBus.$on('login', ()=> { 
+                this.userLoggedIn = User.loggedIn();
+            });
+        },
+
         like(){
             axios.post(`/api/like/${this.reply.id}`)
             .then(res=> {
@@ -59,6 +62,19 @@ export default {
 
             })
             .catch(err=> console.log(err));
+        },
+        setupEchoListeners(){
+            if(window.Echo){
+                Echo.private('likeChannel').listen('.LikeEvent', (e) => {
+                    if(this.reply.id == e.data.reply_id && e.type == 1){
+                        this.reply.likes.push(e.data);
+                    }
+                    else if(this.reply.id == e.data.reply_id && e.type == 0){
+                        const index = this.reply.likes.findIndex(like=> like.user_id == e.data.user_id);
+                        this.reply.likes.splice(index, 1);
+                    }
+                });
+            }
         }
     }
 }

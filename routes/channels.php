4be568309356703117e6e339d626aa41541e884b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-30 20:13:27 -0400

    Add new broadcast channel for deleting replies

--- routes/channels.php
+++ routes/channels.php
@@ -20,3 +20,7 @@ Broadcast::channel('App.User.{id}', function ($user, $id) {
 Broadcast::channel('likeChannel', function () {
     return true;
 });
+
+Broadcast::channel('deleteReplyChannel', function () {
+    return true;
+});

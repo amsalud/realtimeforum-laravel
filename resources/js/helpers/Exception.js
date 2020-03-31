9ec9a97f71605e96d0634bd39d14403939ee3611
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-03-31 13:04:18 -0400

    Provide auth session timeout handling

--- resources/js/helpers/Exception.js
+++ resources/js/helpers/Exception.js
@@ -1,12 +1,18 @@
 import User from './User'
 
 class Exception {
-    handle(err, router){
-        this.isExpired(err.response.data.error, router);
+    handle(err){
+        this.isExpired(err.response.data.error);
     }
 
-    isExpired(err, router){
-        if(err ==  'Token is expired' || err == 'Token is not provided'){
+    isExpired(err){
+        var error_messages =[
+            'Token is expired',
+            'Token is invalid',
+            'Token can not be used, get a new one'
+        ];
+
+        if(error_messages.find( error_message => error_message == err)){
             User.logout();
         }
     }
